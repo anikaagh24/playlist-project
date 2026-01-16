1c74c2bd0c5e626ae60f5bc602bf3bd934923947
anikaagh24 <anikamakter721@gmail.com>
2026-01-16 09:19:32 -0500

    Add files via upload
    I began working on my JS and JSON and added all information needed.

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("js console");                                                                                                                                                           
+
+let data; 
+let grid = document.querySelector(".grid-container");
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status==200){
+        
+
+    data=JSON.parse(xhttp.responseText);
+    console.log(data);
+    localStorage.setItem("dataList", JSON.stringify(data));
+
+    data.forEach(function(movie){ 
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+
+    let TextData = 
+    "<div class = 'movie-title'>" + movie.title + "</div>" + "<span>" + 
+    "Producer: movie.publisher" + "<br>" + "Release Date:" + movie.releaseDate + "<br>" 
+    "</span>";
+
+    card.innerHTML= TextData;
+
+    if (movie.imgSrc){
+        card.style.backgroundImage = "url('" + movie.imgSrc + "')";
+    }
+
+    grid.appendChild(card);
+});
+
+    }
+    };
+
+xhttp.open("GET", "moviedata.json" , true);
+xhttp.send();
